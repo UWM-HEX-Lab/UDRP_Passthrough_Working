8aaac0d73eda66bd064b7f6337d3f7f44802f93c
AlecBrookens <123490227+AlecBrookens@users.noreply.github.com>
2024-01-31 17:03:34 -0600

    package installed

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public enum PathType { SQUARE, CIRCLE, FIGURE8 };
+    public PathType pathType;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
